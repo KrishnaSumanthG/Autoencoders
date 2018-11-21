63785997753f1ac254afa76202b6b5061ae8913d
hypothesis2304 <sumanth2593@gmail.com>
2018-11-21 02:43:17 -0700

    updated train,model

--- De-noising Autoencoder/model.py
+++ De-noising Autoencoder/model.py
@@ -3,247 +3,240 @@ from load_mnist import mnist
 import matplotlib.pyplot as plt
 import pdb
 
-def tanh(Z):
-    '''
-    computes tanh activation of Z
 
-    Inputs: 
-        Z is a numpy.ndarray (n, m)
+class Model():
 
-    Returns: 
-        A is activation. numpy.ndarray (n, m)
-        cache is a dictionary with {"Z", Z}
-    '''
-    A = np.tanh(Z)
-    cache = {}
-    cache["Z"] = Z
-    return A, cache
+    def initialize_2layer_weights(n_in, n_h, n_fin):
 
-def tanh_der(dA, cache):
-    '''
-    computes derivative of tanh activation
+        parameters = {}
+        parameters["W1"] = (np.random.randn(n_in,n_h)*(np.sqrt(2.0/(n_in*n_h)))).T
+        parameters["b1"] = np.zeros(n_h).reshape((n_h,1))
+        parameters["W2"] = (np.random.randn(n_h,n_fin)*(np.sqrt(2.0/(n_h*n_fin)))).T
+        parameters["b2"] = np.zeros(n_fin).reshape((n_fin,1))
 
-    Inputs: 
-        dA is the derivative from subsequent layer. numpy.ndarray (n, m)
-        cache is a dictionary with {"Z", Z}, where Z was the input 
-        to the activation layer during forward propagation
+        return parameters
 
-    Returns: 
-        dZ is the derivative. numpy.ndarray (n,m)
-    '''
-    ### CODE HERE
-    Z = cache["Z"]
-    dZ = dA * (1-np.tanh(Z)**2)
-    return dZ
+    def tanh(Z):
+        '''
+        computes tanh activation of Z
 
-def sigmoid(Z):
-    '''
-    computes sigmoid activation of Z
+        Inputs: 
+            Z is a numpy.ndarray (n, m)
 
-    Inputs: 
-        Z is a numpy.ndarray (n, m)
+        Returns: 
+            A is activation. numpy.ndarray (n, m)
+            cache is a dictionary with {"Z", Z}
+        '''
+        A = np.tanh(Z)
+        cache = {}
+        cache["Z"] = Z
+        return A, cache
 
-    Returns: 
-        A is activation. numpy.ndarray (n, m)
-        cache is a dictionary with {"Z", Z}
-    '''
-    A = 1/(1+np.exp(-Z))
-    cache = {}
-    cache["Z"] = Z
-    return A, cache
+    def tanh_der(dA, cache):
+        '''
+        computes derivative of tanh activation
 
-def sigmoid_der(dA, cache):
-    '''
-    computes derivative of sigmoid activation
+        Inputs: 
+            dA is the derivative from subsequent layer. numpy.ndarray (n, m)
+            cache is a dictionary with {"Z", Z}, where Z was the input 
+            to the activation layer during forward propagation
 
-    Inputs: 
-        dA is the derivative from subsequent layer. numpy.ndarray (n, m)
-        cache is a dictionary with {"Z", Z}, where Z was the input 
-        to the activation layer during forward propagation
+        Returns: 
+            dZ is the derivative. numpy.ndarray (n,m)
+        '''
+        ### CODE HERE
+        Z = cache["Z"]
+        dZ = dA * (1-np.tanh(Z)**2)
+        return dZ
 
-    Returns: 
-        dZ is the derivative. numpy.ndarray (n,m)
-    '''
-    ### CODE HERE
+    def sigmoid(Z):
+        '''
+        computes sigmoid activation of Z
 
-    Z = cache["Z"]
-    A_, cache_= sigmoid(Z)
-    dZ= dA * A_ *(1-A_)
+        Inputs: 
+            Z is a numpy.ndarray (n, m)
 
-    return dZ
+        Returns: 
+            A is activation. numpy.ndarray (n, m)
+            cache is a dictionary with {"Z", Z}
+        '''
+        A = 1/(1+np.exp(-Z))
+        cache = {}
+        cache["Z"] = Z
+        return A, cache
 
-def relu(Z):
-    '''
-    computes relu activation of Z
+    def sigmoid_der(dA, cache):
+        '''
+        computes derivative of sigmoid activation
 
-    Inputs: 
-        Z is a numpy.ndarray (n, m)
+        Inputs: 
+            dA is the derivative from subsequent layer. numpy.ndarray (n, m)
+            cache is a dictionary with {"Z", Z}, where Z was the input 
+            to the activation layer during forward propagation
 
-    Returns: 
-        A is activation. numpy.ndarray (n, m)
-        cache is a dictionary with {"Z", Z}
-    '''
+        Returns: 
+            dZ is the derivative. numpy.ndarray (n,m)
+        '''
+        ### CODE HERE
 
-    A = np.maximum(0,Z)
-    cache = {}
-    cache["Z"] = Z
-    return A, cache
+        Z = cache["Z"]
+        A_, cache_= sigmoid(Z)
+        dZ= dA * A_ *(1-A_)
 
-def relu_der(dA, cache):
-    '''
-    computes derivative of relu activation
+        return dZ
 
-    Inputs: 
-        dA is the derivative from subsequent layer. numpy.ndarray (n, m)
-        cache is a dictionary with {"Z", Z}, where Z was the input 
-        to the activation layer during forward propagation
+    def relu(Z):
+        '''
+        computes relu activation of Z
 
-    Returns: 
-        dZ is the derivative. numpy.ndarray (n,m)
-    '''
-    dZ = np.array(dA, copy=True)
-    Z = cache["Z"]
-    dZ[Z<0] = 0
-    return dZ
+        Inputs: 
+            Z is a numpy.ndarray (n, m)
 
+        Returns: 
+            A is activation. numpy.ndarray (n, m)
+            cache is a dictionary with {"Z", Z}
+        '''
 
-def initialize_2layer_weights(n_in, n_h, n_fin):
-    '''
-    Initializes the weights of the 2 layer network
+        A = np.maximum(0,Z)
+        cache = {}
+        cache["Z"] = Z
+        return A, cache
 
-    Inputs: 
-        n_in input dimensions (first layer)
-        n_h hidden layer dimensions
-        n_fin final layer dimensions
+    def relu_der(dA, cache):
+        '''
+        computes derivative of relu activation
 
-    Returns:
-        dictionary of parameters
-    '''
-    # initialize network parameters
-    ### CODE HERE
+        Inputs: 
+            dA is the derivative from subsequent layer. numpy.ndarray (n, m)
+            cache is a dictionary with {"Z", Z}, where Z was the input 
+            to the activation layer during forward propagation
 
-    parameters = {}
-    parameters["W1"] = (np.random.randn(n_in,n_h)*(np.sqrt(2.0/(n_in*n_h)))).T
-    parameters["b1"] = np.zeros(n_h).reshape((n_h,1))
-    parameters["W2"] = (np.random.randn(n_h,n_fin)*(np.sqrt(2.0/(n_h*n_fin)))).T
-    parameters["b2"] = np.zeros(n_fin).reshape((n_fin,1))
+        Returns: 
+            dZ is the derivative. numpy.ndarray (n,m)
+        '''
+        dZ = np.array(dA, copy=True)
+        Z = cache["Z"]
+        dZ[Z<0] = 0
+        return dZ
 
-    return parameters
+    def linear_forward(A, W, b):
+        '''
+        Input A propagates through the layer 
+        Z = WA + b is the output of this layer. 
 
-def linear_forward(A, W, b):
-    '''
-    Input A propagates through the layer 
-    Z = WA + b is the output of this layer. 
+        Inputs: 
+            A - numpy.ndarray (n,m) the input to the layer
+            W - numpy.ndarray (n_out, n) the weights of the layer
+            b - numpy.ndarray (n_out, 1) the bias of the layer
 
-    Inputs: 
-        A - numpy.ndarray (n,m) the input to the layer
-        W - numpy.ndarray (n_out, n) the weights of the layer
-        b - numpy.ndarray (n_out, 1) the bias of the layer
+        Returns:
+            Z = WA + b, where Z is the numpy.ndarray (n_out, m) dimensions
+            cache - a dictionary containing the inputs A, W and b
+            to be used for derivative
+        '''
+        ### CODE HERE
+        Z=W.dot(A)+b
+        cache = {}
+        cache["A"] = A
+        return Z, cache
 
-    Returns:
-        Z = WA + b, where Z is the numpy.ndarray (n_out, m) dimensions
-        cache - a dictionary containing the inputs A, W and b
-        to be used for derivative
-    '''
-    ### CODE HERE
-    Z=W.dot(A)+b
-    cache = {}
-    cache["A"] = A
-    return Z, cache
+    def layer_forward(A_prev, W, b, activation):
+        '''
+        Input A_prev propagates through the layer and the activation
 
-def layer_forward(A_prev, W, b, activation):
-    '''
-    Input A_prev propagates through the layer and the activation
+        Inputs: 
+            A_prev - numpy.ndarray (n,m) the input to the layer
+            W - numpy.ndarray (n_out, n) the weights of the layer
+            b - numpy.ndarray (n_out, 1) the bias of the layer
+            activation - is the string that specifies the activation function
 
-    Inputs: 
-        A_prev - numpy.ndarray (n,m) the input to the layer
-        W - numpy.ndarray (n_out, n) the weights of the layer
-        b - numpy.ndarray (n_out, 1) the bias of the layer
-        activation - is the string that specifies the activation function
+        Returns:
+            A = g(Z), where Z = WA + b, where Z is the numpy.ndarray (n_out, m) dimensions
+            g is the activation function
+            cache - a dictionary containing the cache from the linear and the nonlinear propagation
+            to be used for derivative
+        '''
+        Z, lin_cache = linear_forward(A_prev, W, b)
+        if activation == "sigmoid":
+            A, act_cache = sigmoid(Z)
+        elif activation == "tanh":
+            A, act_cache = tanh(Z)
+        
+        cache = {}
+        cache["lin_cache"] = lin_cache
+        cache["act_cache"] = act_cache
 
-    Returns:
-        A = g(Z), where Z = WA + b, where Z is the numpy.ndarray (n_out, m) dimensions
-        g is the activation function
-        cache - a dictionary containing the cache from the linear and the nonlinear propagation
-        to be used for derivative
-    '''
-    Z, lin_cache = linear_forward(A_prev, W, b)
-    if activation == "sigmoid":
-        A, act_cache = sigmoid(Z)
-    elif activation == "tanh":
-        A, act_cache = tanh(Z)
-    
-    cache = {}
-    cache["lin_cache"] = lin_cache
-    cache["act_cache"] = act_cache
+        return A, cache
 
-    return A, cache
+    def cost_estimate(A2, Y):
+        '''
+        Estimates the cost with prediction A2
 
-def cost_estimate(A2, Y):
-    '''
-    Estimates the cost with prediction A2
+        Inputs:
+            A2 - numpy.ndarray (1,m) of activations from the last layer
+            Y - numpy.ndarray (1,m) of labels
+        
+        Returns:
+            cost of the objective function
+        '''
+        ### CODE HERE
+        obs = len(Y.T)
+        pred = A2
 
-    Inputs:
-        A2 - numpy.ndarray (1,m) of activations from the last layer
-        Y - numpy.ndarray (1,m) of labels
-    
-    Returns:
-        cost of the objective function
-    '''
-    ### CODE HERE
-    obs = len(Y.T)
-    pred = A2
+        cost= -Y*np.log(pred) - (1-Y)*np.log(1-pred)
+        cost= cost.sum()/obs
 
-    cost= -Y*np.log(pred) - (1-Y)*np.log(1-pred)
-    cost= cost.sum()/obs
+        return cost
 
-    return cost
+    def linear_backward(dZ, cache, W, b):
+        '''
+        Backward propagation through the linear layer
 
-def linear_backward(dZ, cache, W, b):
-    '''
-    Backward propagation through the linear layer
+        Inputs:
+            dZ - numpy.ndarray (n,m) derivative dL/dz 
+            cache - a dictionary containing the inputs A
+                where Z = WA + b,    
+                Z is (n,m); W is (n,p); A is (p,m); b is (n,1)
+            W - numpy.ndarray (n,p)  
+            b - numpy.ndarray (n, 1)
 
-    Inputs:
-        dZ - numpy.ndarray (n,m) derivative dL/dz 
-        cache - a dictionary containing the inputs A
-            where Z = WA + b,    
-            Z is (n,m); W is (n,p); A is (p,m); b is (n,1)
-        W - numpy.ndarray (n,p)  
-        b - numpy.ndarray (n, 1)
+        Returns:
+            dA_prev - numpy.ndarray (p,m) the derivative to the previous layer
+            dW - numpy.ndarray (n,p) the gradient of W 
+            db - numpy.ndarray (n, 1) the gradient of b
+        '''
+        # CODE HERE
+        A =cache["A"]
+        dA_prev = np.dot(W.T,dZ)
+        dW = np.dot(dZ,A.T)
+        db = np.sum(dZ,axis=1,keepdims= True)
+        return dA_prev, dW, db
 
-    Returns:
-        dA_prev - numpy.ndarray (p,m) the derivative to the previous layer
-        dW - numpy.ndarray (n,p) the gradient of W 
-        db - numpy.ndarray (n, 1) the gradient of b
-    '''
-    # CODE HERE
-    A =cache["A"]
-    dA_prev = np.dot(W.T,dZ)
-    dW = np.dot(dZ,A.T)
-    db = np.sum(dZ,axis=1,keepdims= True)
-    return dA_prev, dW, db
+    def layer_backward(dA, cache, W, b, activation):
+        '''
+        Backward propagation through the activation and linear layer
 
-def layer_backward(dA, cache, W, b, activation):
-    '''
-    Backward propagation through the activation and linear layer
+        Inputs:
+            dA - numpy.ndarray (n,m) the derivative to the previous layer
+            cache - dictionary containing the linear_cache and the activation_cache
+            W - numpy.ndarray (n,p)  
+            b - numpy.ndarray (n, 1)
+        
+        Returns:
+            dA_prev - numpy.ndarray (p,m) the derivative to the previous layer
+            dW - numpy.ndarray (n,p) the gradient of W 
+            db - numpy.ndarray (n, 1) the gradient of b
+        '''
+        lin_cache = cache["lin_cache"]
+        act_cache = cache["act_cache"]
 
-    Inputs:
-        dA - numpy.ndarray (n,m) the derivative to the previous layer
-        cache - dictionary containing the linear_cache and the activation_cache
-        W - numpy.ndarray (n,p)  
-        b - numpy.ndarray (n, 1)
-    
-    Returns:
-        dA_prev - numpy.ndarray (p,m) the derivative to the previous layer
-        dW - numpy.ndarray (n,p) the gradient of W 
-        db - numpy.ndarray (n, 1) the gradient of b
-    '''
-    lin_cache = cache["lin_cache"]
-    act_cache = cache["act_cache"]
+        if activation == "sigmoid":
+            dZ = sigmoid_der(dA, act_cache)
+        elif activation == "tanh":
+            dZ = tanh_der(dA, act_cache)
+        dA_prev, dW, db = linear_backward(dZ, lin_cache, W, b)
+        return dA_prev, dW, db
 
-    if activation == "sigmoid":
-        dZ = sigmoid_der(dA, act_cache)
-    elif activation == "tanh":
-        dZ = tanh_der(dA, act_cache)
-    dA_prev, dW, db = linear_backward(dZ, lin_cache, W, b)
-    return dA_prev, dW, db
+    def accuracy(predicted_labels, actual_labels):
+        diff = predicted_labels - actual_labels
+        return (1.0 - (float(np.count_nonzero(diff)) / len(diff.T)))*100
--- De-noising Autoencoder/train.py
+++ De-noising Autoencoder/train.py
@@ -1,120 +1,84 @@
-'''
-This file implements a two layer neural network for a binary classifier
-
-Hemanth Venkateswara
-hkdv1@asu.edu
-Oct 2018
-'''
 import numpy as np
-from load_mnist import mnist
+from load_data import myDataset
+from model import Model
 import matplotlib.pyplot as plt
 import pdb
 
-def two_layer_network(X, Y,X_val,Y_val, net_dims, num_iterations=2000, learning_rate=0.1):
-    '''
-    Creates the 2 layer network and trains the network
+def train(X, Y,X_val,Y_val, net_dims, epochs=2000, learning_rate=0.1):
 
-    Inputs:
-        X - numpy.ndarray (n,m) of training data
-        Y - numpy.ndarray (1,m) of training data labels
-        net_dims - tuple of layer dimensions
-        num_iterations - num of epochs to train
-        learning_rate - step size for gradient descent
-    
-    Returns:
-        costs - list of costs over training
-        parameters - dictionary of trained network parameters
-    '''
-
-    ########################
     n_in, n_h, n_fin = net_dims
-    parameters = initialize_2layer_weights(n_in, n_h, n_fin)
+    model=Model()
+    parameters = model.initialize_2layer_weights(n_in, n_h, n_fin)
     obs = len(Y.T)
     A0 = X
     A0_= X_val
     costs = []
     costs_ = []
-    for ii in range(num_iterations):
-        # Forward propagation
-        ### CODE HERE
-
-        W1,b1 = parameters["W1"],parameters["b1"]
-        W2,b2 = parameters["W2"],parameters["b2"]
-
-        A1,cache1=layer_forward(A0, W1, b1, "tanh")
-        A2,cache2 = layer_forward(A1, W2, b2, "sigmoid")
-
-        # cost estimation
-        ### CODE HERE
-
-        cost = cost_estimate(A2, Y)
-        
-        # Backward Propagation
-        ### CODE HERE
-
-        dA= ((A2 - Y)/ (A2*(1.0 - A2)))/obs
-
-        dA_prev2, dW2, db2 = layer_backward(dA, cache2, W2, b2, "sigmoid")
-        dA_prev1, dW1, db1 = layer_backward(dA_prev2, cache1, W1, b1, "tanh")
-        #print(db1.shape)
-
-        #update parameters
-        ### CODE HERE
-        parameters['W2']+=-learning_rate*dW2
-        parameters['b2']+=-learning_rate*db2
-        parameters['W1']+=-learning_rate*dW1
-        parameters['b1']+=-learning_rate*db1
-
-
-        if ii % 10 == 0:
-            costs.append(cost)
-            A1_,cache1_=layer_forward(A0_, W1, b1, "tanh")
-            A2_,cache2_ = layer_forward(A1_, W2, b2, "sigmoid")
-            cost_ = cost_estimate(A2_, Y_val)
-            costs_.append(cost_)
-
-        if ii % 10 == 0:
-            print("Cost at iteration %i is: %f" %(ii, cost))
     
+    for ii in range(epochs):
+        noBatches = X,shape[0]/batchSize
+        for jj in range(noBatches):
+
+            XTrBatch = myDataset.getTrMiniBatch(batchSize)
+            XValBatch = myDataset.getValMiniBatch(batchSize)
+            
+            W1,b1 = parameters["W1"],parameters["b1"]
+            W2,b2 = parameters["W2"],parameters["b2"]
+
+            A1,cache1= model.layer_forward(XTrBatch, W1, b1, "tanh")
+            A2,cache2 = model.layer_forward(A1, W2, b2, "sigmoid")
+
+            cost = model.cost_estimate(A2, Y)
+
+            dA= ((A2 - Y)/ (A2*(1.0 - A2)))/obs
+
+            dA_prev2, dW2, db2 = model.layer_backward(dA, cache2, W2, b2, "sigmoid")
+            dA_prev1, dW1, db1 = model.layer_backward(dA_prev2, cache1, W1, b1, "tanh")
+
+            parameters['W2']+=-learning_rate*dW2
+            parameters['b2']+=-learning_rate*db2
+            parameters['W1']+=-learning_rate*dW1
+            parameters['b1']+=-learning_rate*db1
+
+
+            if ii % 10 == 0:
+                costs.append(cost)
+                A1_,cache1_= model.layer_forward(XValBatch, W1, b1, "tanh")
+                A2_,cache2_ = model.layer_forward(A1_, W2, b2, "sigmoid")
+                cost_ = model.cost_estimate(A2_, Y_val)
+                costs_.append(cost_)
+
+            if ii % 10 == 0:
+                print("Cost at iteration %i is: %f" %(ii, cost))
+        
+
     return costs,costs_, parameters
 
-def accuracy(predicted_labels, actual_labels):
-    diff = predicted_labels - actual_labels
-    return (1.0 - (float(np.count_nonzero(diff)) / len(diff.T)))*100
-
-def main():
-    # getting the subset dataset from MNIST
-    # binary classification for digits 1 and 7
-    digit_range = [1,7]
+def main(args):
+    digit_range = [""]
     train_data, train_label, val_data, val_label, test_data, test_label = \
-            mnist(noTrSamples=2400,noValSamples=400,noTsSamples=1000,\
+            myDataset(noTrSamples=2400,noValSamples=400,noTsSamples=1000,\
             digit_range=digit_range,\
             noTrPerClass=1200, noValPerClass=200, noTsPerClass=500)
-    
-    #convert to binary labels
-    train_label[train_label==digit_range[0]] = 0
-    train_label[train_label==digit_range[1]] = 1
-    val_label[val_label==digit_range[0]] = 0
-    val_label[val_label==digit_range[1]] = 1
-    test_label[test_label==digit_range[0]] = 0
-    test_label[test_label==digit_range[1]] = 1
 
-    n_in, m = train_data.shape
-    n_fin = 1
+    m, n_in = train_data.shape
+    n_fin = 10
     n_h = 100
     net_dims = [n_in, n_h, n_fin]
     # initialize learning rate and num_iterations
-    learning_rate = 1.0
-    num_iterations = 400
+    args.learning_rate = args.learning_rate
+    epochs = args.epochs
     plot_costs=[]
     plot_costs_=[]
     test_acc=[]
     net_dims = [n_in, n_h, n_fin]
 
-    costs,costs_, parameters = two_layer_network(train_data, train_label, val_data, val_label, net_dims, \
-        num_iterations=num_iterations, learning_rate=learning_rate)
+    costs,costs_, parameters = train(train_data, train_label, val_data, val_label, net_dims, \
+        epochs=epochs, learning_rate=learning_rate)
     plot_costs.append(costs)
     plot_costs_.append(costs_)
+    
+
     # compute the accuracy for training set and testing set
     train_Pred = classify(train_data, parameters)
     test_Pred = classify(test_data, parameters)
@@ -159,7 +123,7 @@ if __name__ == "__main__":
     args.id += '--batchSize' + str(args.batchSize)
     args.id += '--learningRate' + str(args.learningRate)
     
-    main()
+    main(args)
 
 
 
